515b14ebd409c404db9d5e49f2bf7da416bb49a6
Marcin Bojarski <boj.marcin@gmail.com>
2018-03-14 18:24:09 +0100

    Fix scripts at scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -11,7 +11,7 @@ span.each(function(index, element) {
 
 var paragraphs = $('p');
 paragraphs.each(function(index, element) {
-    var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+    var button = '<button class='btn' data-tmp=' + index + '>Click me</button>';
     $(element).append(button);
 });
 
