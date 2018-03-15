f534bf324972d7f3646f216b7e3a8e6313c27e94
Marcin Bojarski <boj.marcin@gmail.com>
2018-03-15 20:29:07 +0100

    Fix scripts at scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,8 @@
-$(function(){
-	console.log('DOM loaded - you can have fun');
-});
-
 var span = $('span');
 span.each(function(index, element) {
 	if(index % 2 == 0) {
 		$(element).css('color', 'red');
-	};
+	}
 });
 
 var paragraphs = $('p');
